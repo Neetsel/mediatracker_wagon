71280495f3d0006a719ab78474fa327b3e01eaa3
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-10 12:12:01 +0100

    plugged books api

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_12_09_093559) do
+ActiveRecord::Schema[7.1].define(version: 2025_12_10_105008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_09_093559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "authors", default: [], array: true
+    t.string "work_id"
+    t.string "book_id"
   end
 
   create_table "chats", force: :cascade do |t|
