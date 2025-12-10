83155ec3b33642ce117ce9163caf17213de19ff8
Julien Balsiere <julien.balsiere@gmail.com>
2025-12-10 11:35:12 +0100

    missing end in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,14 @@ Rails.application.routes.draw do
 
   resources :media, only: [:show, :index] do
     resources :reviews, only: [:new, :create]
-    
+
     member do
       get :reviews
     end
-    
+
     collection do
       post :create_from_omdb
       post :search_from_omdb
     end
   end
+end
