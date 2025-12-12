9c51c34d5156d926e61a15409f759aa53b9ee2c6
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-12 15:11:36 +0100

    fix favorites from other users appearing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,11 @@ class PagesController < ApplicationController
   def likes
     @media = []
     @media_liked = Favorite.like_list
-    @media_liked.each { |liked_medium| @media << Medium.find(liked_medium["favoritable_id"]) }
+    @media_liked.each { |liked_medium|
+      medium = Medium.find(liked_medium["favoritable_id"])
+      if current_user.favorited?(medium)
+        @media << medium
+      end
+    }
   end
 end
