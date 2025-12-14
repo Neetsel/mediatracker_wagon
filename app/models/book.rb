1e00f7467d494f2b492a298639381715ce22e4a2
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-14 15:58:25 +0100

    gros refacto pour pouvoir faire les actions de favoris sur les cards

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,17 +1,17 @@
 class Book < ApplicationRecord
   has_one :media, as: :sub_media
 
-  def self.create_from_medium(author, amount_pages, work_id, book_id)
+  def self.create_from_medium(medium, amount_pages)
     # Si livre existe déjà, on le récupère(cf.doc active record)
-    @Book = Book.find_or_initialize_by(book_id: book_id)
+    @Book = Book.find_or_initialize_by(book_id: medium[:cover_edition_key])
 
     # On met à jour les infos si besoin
     @Book.assign_attributes(
-      book_id: book_id,
-      work_id: work_id,
+      book_id: medium[:cover_edition_key],
+      work_id: medium[:key],
       publisher: "",
       amount_pages: amount_pages,
-      authors: author
+      authors: medium[:author_name]
     )
 
     # on sauve le livre en DB
