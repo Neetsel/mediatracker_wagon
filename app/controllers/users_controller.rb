6791c2b02a20f59bd8c4e6a46ceaeed2c86abdaf
Julien Balsiere <julien.balsiere@gmail.com>
2025-12-15 15:38:54 +0100

    pagination for reviews, likes and next up created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 
   def next_up
     @user = current_user
-    @next_up_media = @user.favorited_by_type("Medium", scope: "next_up")
+    @next_up_media = @user.favorited_by_type("Medium", scope: "next_up").page(params[:page]).per(10)
   end
 end
