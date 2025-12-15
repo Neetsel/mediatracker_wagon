14c6557f5d56cf71a35d68b0341cce85545fcc87
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-15 18:14:41 +0100

    refacto search

---
+++ app/javascript/controllers/next_up_icon_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="next-up-icon"
+export default class extends Controller {
+  connect() {
+  }
+}
--- app/services/igdb_service.rb
+++ app/services/igdb_service.rb
@@ -106,4 +106,75 @@ class IgdbService
     end
   end
 
+  def run(title)
+    # On fait un call API pour récupérer tous les jeux qui portent un nom similaire
+    response = JSON.parse(search_by_title(title).body)
+
+    # On prépare un array qui contient les références de toutes les covers que l on veut récupérer
+    covers_id = []
+    response.each {|game|
+      if game["cover"]
+        covers_id << game["cover"]
+      end
+    }
+
+    # On prépare un array qui contient les références de tous les game_id qui nous serviront à trouver les durées de jeux et les compagnies qui ont bossé dessus
+    games_id = []
+    response.each {|game|
+      if game["id"]
+        games_id << game["id"]
+      end
+    }
+
+    # On fait les calls API nécessaires pour récupérer les covers, les durées de jeux et les compagnies
+    response_cover = JSON.parse(cover_by_id(covers_id).body)
+    response_duration = JSON.parse(duration_by_id(games_id).body)
+    response_companies = JSON.parse(involved_companies_by_id(games_id).body)
+
+    # On modifie la réponse afin de lui ajouter les infos des 3 derniers calls API
+    response.map {|game|
+
+      # On ajoute la cover si le jeu en a une
+      if game["cover"]
+        # On fait le lien entre la ref de game["cover"] et notre réponse de l'API cover
+        cover = response_cover.select { |hash| hash["id"] === game["cover"] }
+        # l'url custom permet d'avoir l image en meilleure résolution
+        game["cover"] = "//images.igdb.com/igdb/image/upload/t_720p/#{cover[0]["image_id"]}.png"
+      end
+
+      # On ajoute la date de sortie si il y en a une
+      if game["first_release_date"]
+        # On convertit la date reçue en Unix pour avoir l'année
+        game["first_release_date"] = DateTime.strptime(game["first_release_date"].to_s, '%s').year
+      end
+
+      # On cherche les informations liées à la complétion du jeu sur base de l'id du jeu
+      time_to_beat = response_duration.select { |hash| hash["game_id"] == game["id"] }
+      # Si on a un résultat, on remplit les infos
+      if time_to_beat[0]
+        game["main_story_duration"] = time_to_beat[0]["hastily"]
+        game["main_extras_duration"] = time_to_beat[0]["normally"]
+        game["completionist_duration"] = time_to_beat[0]["completely"]
+      end
+
+      # On ajoute des champs dévelopeurs et éditeurs au jeu
+      game["developer"] = []
+      game["publisher"] = []
+      # On cherche les informations liées à la création du jeu sur base de l'id du jeu
+      companies = response_companies.select { |hash| hash["game"] == game["id"] }
+      companies.each {|company|
+        # On l'ajoute en tant que développeur si il est listé en tant que tel.
+        if company["developer"]
+          game["developer"] << company["company"]
+        # On l'ajoute en tant qu'éditeur si il est listé en tant que tel.
+        elsif company["publisher"]
+          game["publisher"] << company["company"]
+        end
+      }
+    }
+
+    @results = response.empty? ? [] : response
+    @results
+  end
+
 end
