8a86c9182cbf2862b3c3005d08a81848a309dd2e
Ping Wang <pingwang715@gmail.com>
2025-12-16 15:25:33 +0100

    add profile page with info on collection, like and next up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,11 @@ class UsersController < ApplicationController
     @user = current_user
     @next_up_media = @user.favorited_by_type("Medium", scope: "next_up").page(params[:page]).per(10)
   end
+
+  def stats
+    @user = current_user
+    @collection_media = Medium.joins(:collections).merge(Collection.where(user_id: @user.id)).page(params[:page]).per(10)
+    @next_up_media = @user.favorited_by_type("Medium", scope: "next_up").page(params[:page]).per(10)
+    @like_media = @user.favorited_by_type("Medium", scope: "like").page(params[:page]).per(10)
+  end
 end
